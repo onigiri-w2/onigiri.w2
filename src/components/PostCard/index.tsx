2ebfe2d157e6840bb1b1ddc8981614228234f3b4
onigiri-w2 <94182619+onigiri-w2@users.noreply.github.com>
2025-04-12 02:40:09 +0900

    mod: PostCardのhoverスタイル適用範囲を変更

--- src/components/PostCard/index.tsx
+++ src/components/PostCard/index.tsx
@@ -6,16 +6,11 @@ type Props = {
 }
 export default async function PostCard({ postMetadata }: Props) {
   const { title, date } = postMetadata;
-  // yyyy-MM-ddの形式の日付を日本語の形式に変換する
-  const formattedDate = new Date(date).toLocaleDateString("ja-JP", {
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-  });
+  const formattedDate = new Date(date).toISOString().split('T')[0];
   return (
-    <article className="py-4">
+    <article className="py-4 group cursor-pointer">
       <span className="text-sm text-text-light">{formattedDate}</span>
-      <h2 className="text-lg font-bold hover:underline">{title}</h2>
+      <h2 className="text-lg font-bold group-hover:underline">{title}</h2>
     </article>
   );
 }
