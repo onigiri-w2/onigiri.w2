5843fca46dc27870f1dfac2d432134520f62ad99
onigiri-w2 <94182619+onigiri-w2@users.noreply.github.com>
2025-04-12 12:53:10 +0900

    mod: post一覧の順番を変更する

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 
 
 export default async function Home() {
-  const posts = await getPostMetas();
+  const posts = await getPostMetas("desc");
 
   return (
     <div className='flex flex-col space-y-2'>
