e907e8bd8e2972c31c5ca7e2731749eafec43bf7
onigiri-w2 <94182619+onigiri-w2@users.noreply.github.com>
2025-04-17 14:37:02 +0900

    fix: site_domainの最後のスラッシュを削除

--- src/const.ts
+++ src/const.ts
@@ -1,3 +1,3 @@
 export const POSTS_PATH_FROM_ROOT = '/posts';
 export const AUTHOR_NAME = 'onigiri.w2';
-export const SITE_DOMAIN = 'https://onigiri-w2.pages.dev/';
+export const SITE_DOMAIN = 'https://onigiri-w2.pages.dev';
